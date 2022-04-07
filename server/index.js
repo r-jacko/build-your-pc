e02c1233209dc320fa8bf60532e08f4c9e2c4d48
Jacek Roszak <amigo9569@gmail.com>
2022-04-07 14:56:12 +0200

    cors and json parse fix

--- server/index.js
+++ server/index.js
@@ -2,10 +2,14 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv'
 import listRoutes from './routes/list.js'
+import cors from 'cors'
+import bodyParser from 'body-parser'
 
 dotenv.config();
 const app = express();
 
+app.use(bodyParser.json());
+app.use(cors());
 app.use(listRoutes);
 
 
